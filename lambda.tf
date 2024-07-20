f0ac0449a4e1b7ccce4183f4911fdee31b6a0337
miyama <36231225+hige-dev@users.noreply.github.com>
2024-07-20 15:43:53 +0900

    devide process for slack 3sec limit

--- lambda.tf
+++ lambda.tf
@@ -1,11 +1,11 @@
 data "archive_file" "pw_reset" {
     type = "zip"
     source_dir  = "${path.module}/scripts/pw-reset"
-    output_path = "${path.module}/pw-reset.zip"
+    output_path = "/tmp/pw-reset.zip"
 }
 
 resource "aws_lambda_function" "pw_reset" {
-    filename         = "${path.module}/pw-reset.zip"
+    filename         = "/tmp/pw-reset.zip"
     function_name    = "pw-reset"
     role             = aws_iam_role.pw_reset.arn
     handler          = "lambda_function.lambda_handler"
@@ -33,14 +33,49 @@ resource "aws_cloudwatch_log_group" "pw_reset" {
 }
 
 
+data "archive_file" "pw_reset_exec" {
+    type = "zip"
+    source_dir  = "${path.module}/scripts/pw-reset-exec"
+    output_path = "/tmp/pw-reset-exec.zip"
+}
+
+resource "aws_lambda_function" "pw_reset_exec" {
+    filename         = "/tmp/pw-reset-exec.zip"
+    function_name    = "pw-reset-exec"
+    role             = aws_iam_role.pw_reset.arn
+    handler          = "lambda_function.lambda_handler"
+    runtime          = "python3.12"
+    memory_size      = 128
+    timeout          = 30
+    source_code_hash = data.archive_file.pw_reset_exec.output_base64sha256
+    environment {
+        variables = {
+            "TZ" = "Asia/Tokyo"
+        }
+    }
+}
+
+# resource "aws_lambda_permission" "pw_reset_exec" {
+#     function_name   = aws_lambda_function.pw_reset_exec.arn
+#     principal       = "apigateway.amazonaws.com"
+#     action          = "lambda:InvokeFunction"
+#     source_arn      = "${aws_apigatewayv2_api.pw_reset.execution_arn}/*/*"
+# }
+
+resource "aws_cloudwatch_log_group" "pw_reset_exec" {
+  name              = "/aws/lambda/pw-reset-exec"
+  retention_in_days = 30
+}
+
+
 data "archive_file" "slack_workflow" {
     type = "zip"
     source_dir  = "${path.module}/scripts/slack-workflow"
-    output_path = "${path.module}/slack-workflow.zip"
+    output_path = "/tmp/slack-workflow.zip"
 }
 
 resource "aws_lambda_function" "slack_workflow" {
-    filename         = "${path.module}/slack-workflow.zip"
+    filename         = "/tmp/slack-workflow.zip"
     function_name    = "slack-workflow"
     role             = aws_iam_role.slack_workflow.arn
     handler          = "lambda_function.lambda_handler"
